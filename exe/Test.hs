c83b255cf0abe8d3d90c0efa04a8842b2d7a6862
Felix Clark <mc3690@columbia.edu>
2017-04-25 18:21:49 -0400

    moving test executeable

---
+++ exe/Test.hs
@@ -0,0 +1,13 @@
+import Deck
+import Blackjack
+import Control.Monad.Random (evalRandIO)
+
+main :: IO ()
+main = do
+  let deck = getShuffledDecks 1
+  evalDeck <- evalRandIO deck
+  print evalDeck
+--  let theTable = Table evalDeck [] [] PlayerTurn
+  let theTable = makeTable evalDeck
+  printTable theTable
+--  putStrLn deck
