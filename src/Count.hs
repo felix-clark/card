90633d5b471934712bf568b5c18d82ebdc953ac3
Felix Clark <mc3690@columbia.edu>
2017-05-10 20:37:44 -0400

    weighted draw,

--- src/Count.hs
+++ src/Count.hs
@@ -1,10 +1,12 @@
 -- module for blackjack card-counting functions
+-- {-# LANGUAGE ScopedTypeVariables #-} -- doesn't seem to have the desired effect?
 
 module Count where
 
 import Card
 import Blackjack
 import Data.List (sort, nub)
+import Control.Monad.Random
 
 -- Num: because the count needs to use (+)
 deckCount :: Num a => (Card -> a) -> [Card] -> a
@@ -54,8 +56,8 @@ choose n k
   | k > n      = 0
   | k == 0     = 1
   | k == n     = 1
-  | k > (n `div` 2)  = choose n (n-k)
-  | otherwise  = fromIntegral(n - k + 1) * (choose n (k-1)) `div` (fromIntegral k)
+  | 2*k > n    = choose n (n-k)
+  | otherwise  = (n - k + 1) * (choose n (k-1)) `div` k
 
 -- -- takes first N cards that satisfy a function in a (shuffled) deck
 -- -- and return them with the part of the deck leftover
@@ -64,36 +66,78 @@ choose n k
 -- -- partition p xs == (filter p xs, filter (not . p) xs)
 
 -- lists all possible counts the particular count type can result in for this deck
-possibleCounts :: (Ord a, Num a) => (Card -> a) -> [Card] -> [a]
-possibleCounts countFunc deck = possCounts where
-  possCounts = sort . nub $ countFunc <$> deck
+-- possibly not usefull 
+possCounts :: (Ord a, Num a) => (Card -> a) -> [Card] -> [a]
+possCounts countFunc deck = possCounts where
+  possCounts = sort . nub $ map countFunc deck
+
+-- returns the total count for a whole deck, which may not be 0 for unbalanced counting functions
+totalDeckCount :: (Num a) => (Card -> a) -> [Card] -> a
+totalDeckCount countFunc deck = sum $ map countFunc deck
+-- totalDeckCount = sum . map 
 
 -- this version saves possible counts, along with number of cards providing that count
-possibleCountsWithMax :: (Ord a, Num a) => (Card -> a) -> [Card] -> [(a,Int)]
-possibleCountsWithMax countFunc deck = zip possCounts countsList where
-  deckCounts = countFunc <$> deck
-  possCounts = sort $ nub deckCounts
-  fList = numCountsInDeck <$> possCounts --  :: (Ord a, Num a) => [a -> Int]
-  numCountsInDeck ct dk = length $ filter (== ct) dk 
-  countsList = sequence fList deckCounts
+possCountsWithMax :: (Ord a, Num a) => (Card -> a) -> [Card] -> [(a,Int)]
+possCountsWithMax countFunc deck = zip possibleCounts countsList
+  where
+    deckCounts = map countFunc deck
+    possibleCounts = sort $ nub deckCounts
+    fList = map numCountsInDeck possibleCounts --  :: (Ord a, Num a) => [a -> Int]
+    numCountsInDeck ct dk = length $ filter (== ct) dk 
+    countsList = sequence fList deckCounts
 
 -- return possible sets of numbers of each class of card counts, given a total number of cards and a deck count
-possibleCountSetsWithWeights :: (Eq a, Num a, Fractional b) => Int -> a -> [(a,Int)] -> [(b,[Int])] -- return list of possible quantities of each card class
+possCountSetsFromCountsWithMax :: (Eq a, Num a) => Int -> a -> [(a,Int)] -> [[Int]] -- return list of possible quantities of each card class
 -- so if the possible counts are [-1,0,1] it will return e.g. ([[3,4,2],...[Nm,Nz,Np]...] where [Nm,Nz,Np] satisty Nm + Nz + Np = nCards and Np - Nm - nCount
 -- will need to keep track of counts and max separately, so they are not returned here
-possibleCountSetsWithWeights nCards nCount countWithMaxList = result where
-  countList    = fst <$> countWithMaxList      -- :: [a]
-  countMaxList = snd <$> countWithMaxList                                          :: [Int]
+possCountSetsFromCountsWithMax nCards nCount countWithMaxList = countNumSets where
+  (countList,countMaxList) = unzip countWithMaxList                              -- :: ([a],[Int])
   -- list of length (length possCounts) where each element is a list from 0 to the max allowed for the corresponding count
   countSpread = enumFromTo 0 <$> countMaxList                                       :: [[Int]]
   -- list of lists of length (length possCounts) where [0,3,4] refers to 0 cards of 1st count, 3 cards of 2nd count, etc.
   possResNoConstraint = sequence countSpread                                        :: [[Int]]
   countNumSets = filter countCheckFunc $ filter numCheckFunc possResNoConstraint    :: [[Int]]
-  numCheckFunc   = (nCards ==) . sum                                                :: [Int] -> Bool
-  countCheckFunc = (nCount ==) . sum . zipWith (*) countList . map fromIntegral     :: [Int] -> Bool
-  result = weightFunc countMaxList <$> countNumSets
-  weightFunc :: (Fractional b) => [Int] -> [Int] -> (b, [Int]) -- takes list of max counts, possible set of count numbers, and returns weight with the number of counts
-  weightFunc ctMaxes numsOfCts = (product $ zipWith wfHelp ctMaxes numsOfCts, numsOfCts)
-  wfHelp :: (Fractional b) => Int -> Int -> b
-  -- we need to weight each choice of count numbers by the product of (nMax choose n) for each count possibility
-  wfHelp maxCountNum countNum = recip . fromInteger $ choose (fromIntegral maxCountNum) (fromIntegral countNum)
+    where
+      numCheckFunc   = (nCards ==) . sum                                                :: [Int] -> Bool
+      countCheckFunc = (nCount ==) . sum . zipWith (*) countList . map fromIntegral     :: [Int] -> Bool
+
+-- given list of maximum numbers of each count, and a list of possible sets of counts, provide a list of possible sets of counts with their weights (determined 
+possCountSetsWithWeightsFromMaxAndSets :: [Int] -> [[Int]] -> [(Double,[Int])]
+possCountSetsWithWeightsFromMaxAndSets maxCounts setsOfCounts = weightFunc maxCounts <$> setsOfCounts
+  where
+    -- takes list of max counts, possible set of count numbers, and returns weight with the number of counts
+    weightFunc :: [Int] -> [Int] -> (Double, [Int])
+    weightFunc ctMaxes numsOfCts = (product $ zipWith wfHelp ctMaxes numsOfCts, numsOfCts)
+      where
+        wfHelp :: Int -> Int -> Double
+        -- we need to weight each choice of count numbers by the product of (nMax choose n) for each count possibility
+        wfHelp maxCountNum countNum = fromInteger $ choose (fromIntegral maxCountNum) (fromIntegral countNum)
+
+-- this is the function intended for external use.
+-- takes a counting function, number of cards, intended count, and deck,
+-- returning both the set of possible card counts and a list of sets of numbers of cards w/ each count, along with the weights for each set of possibilities
+possCountSetsWithWeights :: (Num a, Ord a) => ((Card) -> a) -> Int -> a -> [Card] -> ([a],[(Double,[Int])])
+possCountSetsWithWeights countFunc nCards nCount deck = result where
+  countsWithMax = possCountsWithMax countFunc deck
+  (possCounts, maxCounts) = unzip countsWithMax
+  setsOfCounts = possCountSetsFromCountsWithMax nCards nCount countsWithMax
+  countSetsW = possCountSetsWithWeightsFromMaxAndSets maxCounts setsOfCounts
+  result = (possCounts, countSetsW)
+
+-- get a random element given the weights, using the global random number generator
+-- could use Control.Monad.Random.Class.getRandomR or System.Random.randomRIO
+-- seems to work for floating w, but not integral
+weightedDraw :: (Random w, Num w, Ord w) => [(w,a)] -> IO a
+weightedDraw source = do
+  let randRange = (0,foldl (+) 0 $ map fst source) --  :: (w,w)
+  -- let randRange = foldl (\x -> (+) (fst x)) 0 source
+  wtThresh <- getRandomR randRange  --    :: IO w
+  let result = itFunc wtThresh 0 source
+  return result
+    where
+      -- itFunc :: w -> [(w,a)] -> a
+      itFunc wtThresh cumulWeight ((wt,val):ls)
+        | newWeight > wtThresh  = val
+        | otherwise             = itFunc wtThresh newWeight ls
+        where newWeight = cumulWeight + wt
+      itFunc _ _ [] = error "Ran out of weight vals. Might need to just roll the whole function again. Possibly Integral vs. Floating issue"
